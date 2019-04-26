8f2525c80da89b4a9e4102e5f68d2b06839b76e7
coderhawk999 <afroz.cs17@sahyadri.edu.in>
2019-04-26 14:17:50 -0400

    added about and techstack section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,16 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import LandingPage from '../components/LandingPage/index'
+import About from '../components/About/index'
+import Techstacks from '../components/techstacks/index'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
 
     <LandingPage />
-   
+   <About />
+  <Techstacks />
   </Layout>
 )
 
